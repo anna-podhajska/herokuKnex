7c87497be8992e199b329f7cc2b0b9b51d0abe58
Anna P <podhajskaa@gmail.com>
2017-07-23 20:59:21 +1200

    remove primary key from users_likes table

--- migrations/20170723133053_users_likes.js
+++ migrations/20170723133053_users_likes.js
@@ -1,7 +1,6 @@
 // adding new table with users and their likes, many to many:
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("users_likes", (table) => {
-    table.increments("id").primary()
     table.integer("users_id")
     table.integer("likes_id")
   })
