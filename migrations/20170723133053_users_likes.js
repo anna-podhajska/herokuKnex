edcf28774a6bfaa02e662b9551d4d0851e2f05a2
Anna P <podhajskaa@gmail.com>
2017-07-23 20:48:56 +1200

    heroku debug

--- migrations/20170723133053_users_likes.js
+++ migrations/20170723133053_users_likes.js
@@ -1,7 +1,7 @@
 // adding new table with users and their likes, many to many:
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("users_likes", (table) => {
-    table.increments("id")
+    table.increments("id").primary()
     table.integer("users_id")
     table.integer("likes_id")
   })
